7484cc638442b0b38ef3b2f30df4fcc242ac2915
les <chiayin.l.tu@gmail.com>
2013-01-16 18:46:49 -0500

    first commit theme files

---
+++ cielt/parts/shared/header-min.php
@@ -0,0 +1,11 @@
+<header id="page-header" class="header-min">
+<div class="overlay"></div>
+	<div id="wrapper-header-utils" class="clearfix">	
+	<!-- global nav -->
+		<div id="wrapper-global-nav" class="clearfix">
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</div>
+
+	</div>
+
+</header>
